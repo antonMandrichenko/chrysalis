9dcbc47cd752445f71ac78bd07568d203ee79111
Anton Mandrychenko <arnellomutty2@gmail.com>
2019-07-15 17:19:27 +0300

    Change prop in SaveChangesButton (i380)

--- src/renderer/components/SaveChangesButton.js
+++ src/renderer/components/SaveChangesButton.js
@@ -93,7 +93,7 @@ class SaveChangesButton extends React.Component {
       async () => {
         await this.props.onClick(event);
         this.setState({
-          success: !this.props.isUpdate ? this.props.isUpdate : true,
+          success: this.props.isBeginUpdate ? !this.props.isBeginUpdate : true,
           inProgress: false
         });
         this.timer = setTimeout(() => {

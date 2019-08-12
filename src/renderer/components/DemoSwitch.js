3a0220d4a6ec0e3ed0d801c6cf9a95ca493b8cfd
Anton Mandrychenko <arnellomutty2@gmail.com>
2019-08-12 12:47:52 +0300

    Added comments to DemoSwitch (#377)

--- src/renderer/components/DemoSwitch.js
+++ src/renderer/components/DemoSwitch.js
@@ -8,18 +8,34 @@ DemoSwitch.propTypes = {
   isDemo: PropTypes.bool.isRequired
 };
 
+/**
+ * Reactjs functional component that create switch for select demo mode
+ * @param {function} onAddMockKeyboards Callback function from KeyboardSelect component. Parameter is checked of Switch component
+ * @param {boolean} isDemo if in demo mode isDemo === true
+ */
 function DemoSwitch(props) {
   const { onAddMockKeyboards, isDemo } = props;
+
+  /**
+   * This is Hook that lets add React state "state.checked" to functional components
+   * @param {object} [checked] Checked default value = false
+   */
   const [state, setState] = useState({
     checked: false
   });
 
+  /**
+   * Changes "state.checked", if prop isDemo === true
+   */
   useEffect(() => {
     if (isDemo) {
       setState({ checked: isDemo });
     }
   }, []);
 
+  /**
+   * Changes switch state and calls onAddMockKeyboards method from KeyboardSelect
+   */
   const handleChange = name => async e => {
     setState({ [name]: e.target.checked });
     onAddMockKeyboards(state.checked);

aad315502b7052fc0f8a0cd496ad555e9058405a
anton-voron <anton.voron.gt@gmail.com>
2019-09-10 18:19:28 +0300

    Define props, update style and UI compon.  (i16)

--- src/renderer/components/SelectLanguage/SelectLanguage.js
+++ src/renderer/components/SelectLanguage/SelectLanguage.js
@@ -1,15 +1,39 @@
+// -*- mode: js-jsx -*-
+/* Chrysalis -- Kaleidoscope Command Center
+ * Copyright (C) 2018, 2019  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import { withStyles } from "@material-ui/core/styles";
+// languagesDB is data base, typeof object
+import { languagesDB } from "@chrysalis-api/keymap";
 import Button from "@material-ui/core/Button";
 import TranslateIcon from "@material-ui/icons/Translate";
-import Menu from "@material-ui/core/Menu";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import LanguageItem from "./LanguageItem";
-import { languagesDB } from "@chrysalis-api/keymap";
+import Popper from "@material-ui/core/Popper";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
 
 const styles = theme => ({
   root: {
-    position: "relative"
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
   },
   button: {
     margin: 15,
@@ -19,12 +43,17 @@ const styles = theme => ({
     marginLeft: theme.spacing.unit * 1
   },
   menu: {
-    position: "absolute",
-    bottom: 300,
-    width: 250,
-    minHeight: 30
+    width: "100%"
   }
 });
+
+/**
+ * Reactjs class component that select new language layout
+ * @param {object} classes Property that sets up CSS classes that adding to HTML elements
+ * @param {HTMLAnchorElement} anchorEl HTMLAnchorElement which is a relative element for our list of languages
+ * @param {string} language String value, that passes the value to the LanguageItem component to select language layout
+ * @param {function} scanKeyboard Callback function from Editor -> KeySelector components. Without parametrs, this function call KeymapDB in Keymap and modify languagu layout
+ */
 
 class SelectLanguage extends Component {
   state = {
@@ -33,7 +62,7 @@ class SelectLanguage extends Component {
   };
   handleOpenLanguage = evt => {
     this.setState({
-      anchorEl: evt.target
+      anchorEl: evt.currentTarget
     });
   };
 
@@ -54,41 +83,50 @@ class SelectLanguage extends Component {
     const { anchorEl, language } = this.state;
     const open = Boolean(anchorEl);
     const languageList = Object.keys(languagesDB).map(item => (
-      <LanguageItem
-        key={item.toString()}
-        language={item}
-        onClose={this.handleCloseLanguage}
-        languageSelect={this.languageSelect}
-        scanKeyboard={scanKeyboard}
-      >
-        {item}
-      </LanguageItem>
+      <div key={item.toString()}>
+        <LanguageItem
+          language={item}
+          onClose={this.handleCloseLanguage}
+          languageSelect={this.languageSelect}
+          scanKeyboard={scanKeyboard}
+        >
+          {item}
+        </LanguageItem>
+        <Divider />
+      </div>
     ));
 
     return (
-      <div id="select-language">
+      <div>
         <Button
           variant="contained"
-          color="secondary"
+          color="default"
           className={classes.button}
           onClick={this.handleOpenLanguage}
         >
           {language}
           <TranslateIcon className={classes.rightIcon} />
         </Button>
-        <Menu
-          id="long-menu"
-          anchorEl={anchorEl}
-          keepMounted
+        <Popper
           open={open}
-          onClose={this.handleCloseLanguage}
-          className={classes.menu}
+          anchorEl={anchorEl}
+          placement="top-start"
+          disablePortal={true}
         >
-          {languageList}
-        </Menu>
+          <ClickAwayListener onClickAway={this.handleCloseLanguage}>
+            <List component="nav" className={classes.root}>
+              {languageList}
+            </List>
+          </ClickAwayListener>
+        </Popper>
       </div>
     );
   }
 }
+
+SelectLanguage.propTypes = {
+  classes: PropTypes.object.isRequired,
+  scanKeyboard: PropTypes.func.isRequired
+};
 
 export default withStyles(styles)(SelectLanguage);

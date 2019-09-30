3042bdd83d76ef677ff7837394195a8f3f7018b4
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-09-30 14:27:00 +0300

    Created MacrosCardInput (macros)

---
+++ src/renderer/components/macros/MacrosTab/MacrosCardInput.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  }
+});
+
+function MacrosCardInput(props) {
+  const [name, setName] = useState("macros 1");
+  const handleChange = e => {
+    setName(e.target.value);
+  };
+
+  const { classes } = props;
+
+  return (
+    <form className={classes.container} noValidate autoComplete="off">
+      <TextField
+        id="macros-name"
+        label="Macros name"
+        className={classes.textField}
+        value={name}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+        fullWidth
+      />
+    </form>
+  );
+}
+
+MacrosCardInput.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(MacrosCardInput);

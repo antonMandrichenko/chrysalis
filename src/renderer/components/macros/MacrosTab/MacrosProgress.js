136a1822d81b9c6be43d91f26a930faa4b60a55a
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-09-30 14:27:22 +0300

    Created macrosProgress (macros)

---
+++ src/renderer/components/macros/MacrosTab/MacrosProgress.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const styles = () => ({
+  root: {
+    flexGrow: 1,
+    padding: 10,
+    marginTop: "1rem"
+  },
+  div: {
+    position: "relative"
+  },
+  progress: {
+    height: 20
+  },
+  percentage: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+});
+
+function MacrosProgress(props) {
+  const { classes } = props;
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+    function progress() {
+      setCompleted(oldCompleted => {
+        if (oldCompleted === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldCompleted + diff, 100);
+      });
+    }
+
+    const timer = setInterval(progress, 500);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Paper className={classes.root}>
+      <Typography gutterBottom>Used macros memory</Typography>
+      <div className={classes.div}>
+        <LinearProgress
+          color="secondary"
+          variant="determinate"
+          value={completed}
+          className={classes.progress}
+        />
+        <Typography className={classes.percentage}>{`${Math.round(
+          completed
+        )}%`}</Typography>
+      </div>
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(MacrosProgress);

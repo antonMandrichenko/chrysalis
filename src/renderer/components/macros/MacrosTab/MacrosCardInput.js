23b92c0cdf259677069703622e0c6efa0f3e0dbb
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-10-02 14:58:26 +0300

    Deleted not needed parameters in styles (macros)

--- src/renderer/components/macros/MacrosTab/MacrosCardInput.js
+++ src/renderer/components/macros/MacrosTab/MacrosCardInput.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
-const styles = theme => ({
+const styles = () => ({
   container: {
     display: "flex",
     flexWrap: "wrap"

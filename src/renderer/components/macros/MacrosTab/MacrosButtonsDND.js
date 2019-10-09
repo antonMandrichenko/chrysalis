e0bd22ca867971a1b98b5eb04403469191e12086
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-10-09 19:03:58 +0300

    Added licence in top of components (macros)

--- src/renderer/components/macros/MacrosTab/MacrosButtonsDND.js
+++ src/renderer/components/macros/MacrosTab/MacrosButtonsDND.js
@@ -1,3 +1,19 @@
+// -*- mode: js-jsx -*-
+/* Chrysalis -- Dygma Raise macros
+ * Copyright (C) 2019  DygmaLab SE
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import React, { useState } from "react";
 // import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
@@ -10,7 +26,7 @@ const propTypes = {};
 
 const styles = {
   root: {
-    maxHeight: 500,
+    maxHeight: 400,
     overflow: "auto",
     paddingBottom: 25,
     marginLeft: 15,
@@ -54,6 +70,7 @@ const MacrosButtonsDND = props => {
     deleteKeyFromMacros,
     openKeyConfig,
     openDelayConfig
+    // currentLanguageLayout
   } = props;
   const [state, setState] = useState(
     macros.data.map((item, i) => `${item} ${i}`)
--- src/renderer/components/macros/MacrosTab/MacrosTab.js
+++ src/renderer/components/macros/MacrosTab/MacrosTab.js
@@ -1,3 +1,19 @@
+// -*- mode: js-jsx -*-
+/* Chrysalis -- Dygma Raise macros
+ * Copyright (C) 2019  DygmaLab SE
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
